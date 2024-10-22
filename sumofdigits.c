c5732c4fed45deb5d3740268722b964da370d80c
Anirudh-2046 <anirudhgupta2046@gmail.com>
2024-10-22 20:38:41 +0530

    Add files via upload

---
+++ sumofdigits.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,rem,sum=0;
+    printf("enter the number:");
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        rem=n%10;
+        sum=sum+rem;
+        n=n/10;
+    }    
+    printf("sum of numbers=%d",sum);
+    return 0;
+}
