aea98fa450d85c88d105362053c1dd2473ca8c09
Anirudh-2046 <anirudhgupta2046@gmail.com>
2024-11-11 14:45:36 +0530

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,21 +1,15 @@
 #include<stdio.h>
-#include<stdlib.h>
 int main()
 {
-    system("cls");
-    int n,sum=0;
-    printf("Enter the 5 digit number");
-    scanf("%d",&n);
-    sum+=n%10;
-    n=n/10;
-    sum+=n%10;
-    n=n/10;
-    sum+=n%10;
-    n=n/10;
-    sum+=n%10;
-    n=n/10;
-    sum+=n%10;
-    n=n/10;
-    printf("sum of digits =%d",sum);
-    return 0;   
+    int arr[4][3]={{17,34,23},{67,23,19},{81,37,42},{52,39,40}};
+    printf("elements of 2D array are:\n");
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
